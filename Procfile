95de99e7677c080580529cd58b39f4fe80d52c50
nicolaspalacios-f <nicolas.palacios-f@mail.escuelaing.edu.co>
2022-06-28 10:35:38 -0500

    heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/tablero-interactivo-buenas-practicas-1.0-SNAPSHOT.jar
---
+++ src/main/java/edu/escuelaing/arsw/ASE/app/BBAppStarter.java
@@ -0,0 +1,11 @@
+package edu.escuelaing.arsw.ASE.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BBAppStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(BBAppStarter.class, args);
+    }
+}
